2fdf902cae924103d1de8da13291250ac24a671f
kannyrithvik-code <kannyrithvik@gmail.com>
2026-08-25 15:32:25 +0530

    Enable Swagger in production

--- SriKaaliYouth.Api/SriKaaliYouth.Api/Program.cs
+++ SriKaaliYouth.Api/SriKaaliYouth.Api/Program.cs
@@ -148,11 +148,10 @@ using (var scope = app.Services.CreateScope())
     }
 }
 
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 
 app.UseHttpsRedirection();
 
